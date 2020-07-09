24df54b0279c38e97942ec40bdd68afa3437f7c8
jonniebigodes <jonniebigodes@hotmail.com>
2020-07-09 17:57:13 +0100

    accessibility added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,19 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  // stories: ['../src/**/*.stories.js'],
+  // addons: [
+  //   '@storybook/preset-create-react-app',
+  //   '@storybook/addon-actions',
+  //   '@storybook/addon-links',
+  //   '@storybook/addon-storysource',
+  //   '@storybook/addon-knobs',
+  // ],
+  stories: ["../src/**/*.stories.(js|mdx)"],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-storysource',
-    '@storybook/addon-knobs',
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-knobs",
+    "@storybook/addon-a11y",
   ],
 };
